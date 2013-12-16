28f5c92e1ceab6fd085b5af98c4acefb2523a80c
Dave Powers <djpowers89@gmail.com>
2013-12-16 11:28:19 -0500

    Implement new sale creation feature

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,5 +1,9 @@
 class SalesController < ApplicationController
 
+  helper_method :employees_dropdown_list
+  helper_method :customers_dropdown_list
+  helper_method :products_dropdown_list
+
   def index
     @sales = Sale.all
     @sales_last_12_months = Sale.sales_last_12_months
@@ -7,30 +11,34 @@ class SalesController < ApplicationController
 
   def new
     @sale = Sale.new
-    @employees_dropdown_list = Employee.return_employee_names_and_ids
-    @customers_dropdown_list = Customer.return_customer_names_and_ids
-    @products_dropdown_list = Product.return_product_names_and_ids
   end
 
   def create
     @sale = Sale.new(sale_params)
 
-    respond_to do |format|
-      if @sale.save
-        format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @sale }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @sale.errors, status: :unprocessable_entity }
-      end
+    if @sale.save
+      redirect_to sales_path, notice: 'Sale was successfully created.'
+    else
+      render action: 'new'
     end
   end
-
 
   private
 
   def sale_params
     params.require(:sale).permit(:sale_date, :sale_amount, :units_sold, :invoice_no, :invoice_frequency, :employee_id, :customer_id, :product_id)
+  end
+
+  def employees_dropdown_list
+    @employee_dropdowm_list ||= Employee.return_employee_names_and_ids
+  end
+
+  def customers_dropdown_list
+    @customers_dropdown_list ||= Customer.return_customer_names_and_ids
+  end
+
+  def products_dropdown_list
+    @products_dropdown_list ||= Product.return_product_names_and_ids
   end
 
 end
